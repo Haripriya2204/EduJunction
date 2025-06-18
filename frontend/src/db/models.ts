d73305cfc72b2ed5ceb806f8ab460179df5deb14
Haripriya2204 <donepudiharipriya2204@gmail.com>
2025-06-18 10:10:35 +0530

    added a detained student

--- frontend/src/db/models.ts
+++ frontend/src/db/models.ts
@@ -100,7 +100,7 @@ export interface OpenElective {
 
 // List of detained students (comma-separated roll numbers)
 export const DETAINED_STUDENTS =
-  "23R21A12B3,2020CS002,2020CS003,2020CS004,2020CS005";
+  "23R21A12B3,2020CS002,2020CS003,2020CS004,2020CS005,22R21A66D4";
 
 /**
  * SQLite table creation statements - for reference only
