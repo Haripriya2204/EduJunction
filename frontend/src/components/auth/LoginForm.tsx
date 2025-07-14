3bb0fba8725248fd431c702b239f9618cc3d2b00
KeEbEe123 <keertan.k@gmail.com>
2025-07-14 23:14:33 +0530

    bday commit - turned off student logins

--- frontend/src/components/auth/LoginForm.tsx
+++ frontend/src/components/auth/LoginForm.tsx
@@ -6,7 +6,7 @@ import { toast } from "sonner";
 import { supabase } from "../../lib/supabase";
 
 // Flag to temporarily disable student logins
-const STUDENT_LOGINS_DISABLED = false;
+const STUDENT_LOGINS_DISABLED = true;
 import {
   Form,
   FormControl,
